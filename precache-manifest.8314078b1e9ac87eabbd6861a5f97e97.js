04e4cd642d2dbd0d4a24fbed65e5fa71f8345351
nikhilunni511 <nikhi.unni@gmail.com>
2020-10-01 19:16:40 +0530

    pages

---
+++ precache-manifest.8314078b1e9ac87eabbd6861a5f97e97.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae946d280efc80fe28b353cc703d6184",
+    "url": "/index.html"
+  },
+  {
+    "revision": "066e51db6c3332a535c7",
+    "url": "/static/css/main.72204a5d.chunk.css"
+  },
+  {
+    "revision": "e9cde263695963288c79",
+    "url": "/static/js/2.d6c82096.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d6c82096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066e51db6c3332a535c7",
+    "url": "/static/js/main.23a4a5a5.chunk.js"
+  },
+  {
+    "revision": "7439f146849e064a2b35",
+    "url": "/static/js/runtime-main.120c8271.js"
+  },
+  {
+    "revision": "6dab26cc33ac73c7e326e489fcab0f26",
+    "url": "/static/media/Donlon.6dab26cc.png"
+  },
+  {
+    "revision": "151b47d906353a50a5900043e43d6d1f",
+    "url": "/static/media/Jebing.151b47d9.png"
+  },
+  {
+    "revision": "1c884377381bbe435920ab2521d34003",
+    "url": "/static/media/Sapir.1c884377.png"
+  }
+]);
