73c9e96a7c82cb0dfa290d89ae25a79a7f4c2222
nikhilunni511 <nikhi.unni@gmail.com>
2020-10-01 20:34:29 +0530

    Updates

---
+++ precache-manifest.bfdc59d14b0a06ab118d6a83cc30eda7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57904125bc507df788e1f2046165e37c",
+    "url": "/finding-falcone/index.html"
+  },
+  {
+    "revision": "facab499e12bac68261c",
+    "url": "/finding-falcone/static/css/main.b99072aa.chunk.css"
+  },
+  {
+    "revision": "9de3133245662972e2e2",
+    "url": "/finding-falcone/static/js/2.b034cb96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/finding-falcone/static/js/2.b034cb96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "facab499e12bac68261c",
+    "url": "/finding-falcone/static/js/main.c83b0a91.chunk.js"
+  },
+  {
+    "revision": "24a4de3582ab34c9fe50",
+    "url": "/finding-falcone/static/js/runtime-main.9ae9d0cf.js"
+  },
+  {
+    "revision": "6dab26cc33ac73c7e326e489fcab0f26",
+    "url": "/finding-falcone/static/media/Donlon.6dab26cc.png"
+  },
+  {
+    "revision": "151b47d906353a50a5900043e43d6d1f",
+    "url": "/finding-falcone/static/media/Jebing.151b47d9.png"
+  },
+  {
+    "revision": "1c884377381bbe435920ab2521d34003",
+    "url": "/finding-falcone/static/media/Sapir.1c884377.png"
+  }
+]);
